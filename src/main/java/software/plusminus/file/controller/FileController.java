6bbc1aa928e68ed4414bf6e5464c881e96c6e25c
Taras Shpek <taras.shpek@gmail.com>
2021-08-07 23:59:49 +0300

    add file controller

---
+++ src/main/java/software/plusminus/file/controller/FileController.java
@@ -0,0 +1,37 @@
+package software.plusminus.file.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import software.plusminus.fs.service.FileService;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+@Controller
+@RequestMapping("/files")
+public class FileController {
+
+    @Autowired
+    private FileService service;
+
+    @GetMapping("/{path}")
+    @ResponseBody
+    public InputStream download(@PathVariable String path) {
+        return service.download(path);
+    }
+
+    @PostMapping("/{path}")
+    @ResponseBody
+    public URL upload(@PathVariable String path,
+                      MultipartFile file) throws IOException {
+        return service.upload(path + '/' + file.getOriginalFilename(),
+                file.getInputStream());
+    }
+}
